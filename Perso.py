e6cbe2d2b04aab9d29818ac77c438f1b1bdcd06b
Esteban Dutrannois <esteban.dutrannois@outlook.com>
2025-05-16 16:20:59 +0200

    Merge personnage in main with merge conflict

--- Perso.py
+++ Perso.py
@@ -4,3 +4,10 @@ print('perso')
 
 # Sur ma branche Main, je n'ai pas encore le fichier Perso.py car je n'ai pas encore fait de merge
 # JE re push ce que je viens d'écrire
+
+# Voilà le merge conflict, nous avons donc 3 choix:
+# 1. Garder la version de la branche Main (Current change), print("Je suis sur la branche Main")
+# 2. Garder la version de la branche Personnage (Incoming change)? print("Je suis sur la branche Personnage")
+# 3. Garder les deux versions(Both change), c'est ce que nous allons faire
+
+# Une fois réglé ne pas oublier de cliquer sur le +
