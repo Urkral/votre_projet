95fa96c735d42fd8d63811facbcb84cb50d3c985
Esteban Dutrannois <esteban.dutrannois@outlook.com>
2025-05-16 16:15:35 +0200

    Feat: commentaire + merge personnage

--- Main.py
+++ Main.py
@@ -3,3 +3,4 @@ print("Coucou")
 #Je fais un simple print
 #Le code se retrouve sur github
 #Je vais maintenant créer une nouvelle branche pour travailler sur une nouvelle fonctionnalité
+#Je retourne sur ma branche Main, et je vais merge ma branche Personnage dedans 
