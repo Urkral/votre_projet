dfbc1ed9d9f29b90a30eea1a16e1b51ae0f13b97
Esteban Dutrannois <esteban.dutrannois@outlook.com>
2025-05-16 16:18:00 +0200

    Feat: print "Je suis sur la branche Personnage"

--- Main.py
+++ Main.py
@@ -3,3 +3,13 @@ print("Coucou")
 #Je fais un simple print
 #Le code se retrouve sur github
 #Je vais maintenant créer une nouvelle branche pour travailler sur une nouvelle fonctionnalité
+
+
+#Donc si l'un de vos collègues travail ici sans vous prévenir, il va faire un commit et un push
+#Vous allez ensuite le merge dans votre branche et créer un merge conflict
+
+print("Je suis sur la branche Personnage")
+print("Je suis sur la branche Personnage")
+print("Je suis sur la branche Personnage")
+print("Je suis sur la branche Personnage")
+print("Je suis sur la branche Personnage")
