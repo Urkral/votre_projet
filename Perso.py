ecd006958ec8bad2e769b0dab2c883e4c2fe4d0f
Esteban Dutrannois <esteban.dutrannois@outlook.com>
2025-05-16 16:14:45 +0200

    Feat: commentaire

--- Perso.py
+++ Perso.py
@@ -1,3 +1,6 @@
 print('perso')
 # Ici je travaille sur ma fonctionnalité, car un ne travaille jamais directement sur le main
 # Je commit et je push ma fonctionnalité
+
+# Sur ma branche Main, je n'ai pas encore le fichier Perso.py car je n'ai pas encore fait de merge
+# JE re push ce que je viens d'écrire
